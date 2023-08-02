e0761cad8907cccf8b37df9b9f0968fa79dc997a
edten2015 <edten2015@gmail.com>
2023-08-01 20:20:47 -0400

    java commit

---
+++ MyProject/src/my/pack/Test.java
@@ -0,0 +1,14 @@
+package my.pack;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  
+		System.out.println("Hello Eclipse");
+		System.out.println("Hello Eclipse");
+		System.out.println("Hello Eclipse");
+		System.out.println("Hello Eclipse");
+	}
+
+}
