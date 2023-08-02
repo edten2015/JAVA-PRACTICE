d05b1d8e81de3815e9f14b94c607ab9a554ae58a
edten2015 <edten2015@gmail.com>
2023-08-02 17:58:37 -0400

    updated

--- MyProject/src/my/pack/Test.java
+++ MyProject/src/my/pack/Test.java
@@ -1,15 +1,24 @@
+
 package my.pack;
+import java.util.Scanner;  
 
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		 Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+		    System.out.println("Enter First Name:  ");
+
+		    String Name = myObj.nextLine();  // Read user input
+		    
+		    System.out.println("Enter Last Name:  ");
+		    
+		    String LName = myObj.nextLine();
+		    
+		    System.out.println("Your Name is:  " + Name + "" + LName);  // Output user input
   
-		System.out.println("Hello Eclipse");
-		System.out.println("Hello Eclipse");
-		System.out.println("Hello Eclipse");
-		System.out.println("Hello Eclipse");
-		System.out.println("Hello Eclipse");
+		
 	}
 
 }
